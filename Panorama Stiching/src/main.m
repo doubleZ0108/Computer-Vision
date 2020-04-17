0f530169a4385ccecc43191a9028d5f36a534626
double Z <doubleZ0108@163.com>
2020-04-17 22:07:26 +0800

    SIFT & Descriptor

--- Panorama Stiching/src/main.m
+++ Panorama Stiching/src/main.m
@@ -8,7 +8,7 @@ file_name_l = 'mountain1.jpg'; file_name_r = 'mountain2.jpg';
 
 img_l = imread([file_path file_name_l]); img_r = imread([file_path file_name_r]);
 
-[M, N, dummy] = size(img_l);
+[M, N, ~] = size(img_l);
 img_r = imresize(img_r, [M, N]);  % match the size of two images
 
 SIFT(img_l, img_r);
